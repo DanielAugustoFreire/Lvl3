e8c30707b993a0c35f0307464982a0c9a8fb5dc5
DanielAugustoSant <danielaugustosant@hotmail.com>
2023-05-01 12:32:49 -0300

    Create lvl3.c

---
+++ lvl3.c
@@ -0,0 +1,13 @@
+/*Claro! Aqui está um exercício de nível 3 que envolve recursão:
+
+Escreva uma função recursiva que calcule o módulo de um número 
+inteiro a, dividido por um número inteiro b, sem usar o operador 
+de módulo (%). Os números a e b devem ser lidos do usuário. A função 
+deve retornar o resultado do cálculo. Por exemplo, se a = 15 e b = 4,
+ a função deve retornar 3, que é o módulo de 15 dividido por 4 (15 = 4 x 3 + 3).*/
+ 3
+
+
+
+
+ 
