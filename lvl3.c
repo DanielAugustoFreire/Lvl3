20f87065d6696aabfa2f2dc24fe3c8c350f7518b
DanielAugustoSant <danielaugustosant@hotmail.com>
2023-05-04 13:54:04 -0300

    Update lvl3.c

--- lvl3.c
+++ lvl3.c
@@ -5,9 +5,24 @@ inteiro a, dividido por um número inteiro b, sem usar o operador
 de módulo (%). Os números a e b devem ser lidos do usuário. A função 
 deve retornar o resultado do cálculo. Por exemplo, se a = 15 e b = 4,
  a função deve retornar 3, que é o módulo de 15 dividido por 4 (15 = 4 x 3 + 3).*/
- 3
 
 
+int modular(int a, int b);
 
+int main(void)
+{
+    int num, num2;
 
- 
+    printf("Entre com o numero que sera dividido:");
+    scanf("%d", &num);
+    printf("Entre com o numero que dividira:");
+    scanf("%d", &num2);
+
+    printf("O resultado do modulo de %d e %d eh ## %d ##", num, num2, modular(num, num2));
+
+}
+
+int modular(int a, int b)
+{
+    
+}
